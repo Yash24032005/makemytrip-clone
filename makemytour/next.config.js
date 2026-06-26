ae08e601a920a143045812afe3dd8a20a853de91
Yash Bansal <byash8587@gmail.com>
2026-06-26 20:48:11 +0530

    fix: final proxy rewrite setup

--- makemytour/next.config.js
+++ makemytour/next.config.js
@@ -6,7 +6,23 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/flight/:path*',
+        destination: 'https://makemytrip-clone-17hl.onrender.com/flight/:path*',
+      },
+      {
+        source: '/hotel/:path*',
+        destination: 'https://makemytrip-clone-17hl.onrender.com/hotel/:path*',
+      },
+      {
+        source: '/user/:path*',
+        destination: 'https://makemytrip-clone-17hl.onrender.com/user/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
